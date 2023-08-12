db0fb32e1869df28f2b7c83426098e4702c48b3d
EvoShishir <atef.arman@yahoo.com>
2023-08-12 21:31:48 +0600

    added ui for posting and showing reviews

--- src/Pages/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProductPage/SingleProductPage.jsx
@@ -8,17 +8,23 @@ import { ADD_TO_CART } from "../../Redux/typings/reducerTypings";
 import { useDispatch } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 
 const SingleProductPage = () => {
   const [product, setProduct] = useState({});
   const [quantity, setQuantity] = useState(1);
   const [activeImage, setActiveImage] = useState({});
+
+  const [value, setValue] = React.useState(0);
+
   const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     fetchProductData();
+    window.scrollTo(0, 0);
   }, []);
 
   const fetchProductData = async () => {
@@ -56,51 +62,126 @@ const SingleProductPage = () => {
   return (
     <Layout>
       <ToastContainer />
-      <div className="single-product">
-        <div className="product-image">
-          <img
-            src={`http://localhost:4000/images/${activeImage}`}
-            alt=""
-            draggable="false"
-            className="image"
-          />
-          <div className="additional-images">
-            {product.images?.map((image, key) => (
-              <img
-                src={image}
-                alt=""
-                key={key}
-                draggable="false"
-                onClick={() => setActiveImage(image)}
-              />
-            ))}
+      <div>
+        <div className="single-product">
+          <div className="product-image">
+            <img
+              src={`http://localhost:4000/images/${activeImage}`}
+              alt=""
+              draggable="false"
+              className="image"
+            />
+            <div className="additional-images">
+              {product.images?.map((image, key) => (
+                <img
+                  src={image}
+                  alt=""
+                  key={key}
+                  draggable="false"
+                  onClick={() => setActiveImage(image)}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="product-info">
+            <p className="product-name-single">{product.name}</p>
+            <p className="product-price">${product.price}</p>
+            <div className="quantity">
+              <button
+                className="selector-btn"
+                onClick={() => handleQuantity("-")}
+              >
+                -
+              </button>
+              <p className="product-quantity">{quantity}</p>
+              <button
+                className="selector-btn"
+                onClick={() => handleQuantity("+")}
+              >
+                +
+              </button>
+            </div>
+            <Button
+              title="Add to Cart"
+              onClick={() => handleAddToCart(product)}
+            />
+
+            <div className="product-description">
+              <p className="description">{product.description}</p>
+            </div>
           </div>
         </div>
-        <div className="product-info">
-          <p className="product-name-single">{product.name}</p>
-          <p className="product-price">${product.price}</p>
-          <div className="quantity">
-            <button
-              className="selector-btn"
-              onClick={() => handleQuantity("-")}
+        <h1
+          style={{
+            marginLeft: "100px",
+          }}
+        >
+          Like your purchase? Leave a review!
+        </h1>
+        <div className="review-form">
+          <div className="review-rating">
+            <h3 style={{ marginBottom: "10px" }}>Rate the product:</h3>
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
             >
-              -
-            </button>
-            <p className="product-quantity">{quantity}</p>
-            <button
-              className="selector-btn"
-              onClick={() => handleQuantity("+")}
-            >
-              +
-            </button>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </Box>
           </div>
-          <Button
-            title="Add to Cart"
-            onClick={() => handleAddToCart(product)}
-          />
+          <div>
+            <h3>Say something about the product:</h3>
+            <textarea />
+          </div>
+          <Button title="Post Review" />
+        </div>
+        <h1
+          style={{
+            marginLeft: "100px",
+          }}
+        >
+          Reviews(4)
+        </h1>
 
-          <div className="product-description">
-            <p className="description">{product.description}</p>
+        <div className="review-container">
+          <div className="review">
+            <h3>Shishir</h3>
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
+            >
+              <Rating name="read-only" value={5} readOnly />
+            </Box>
+            <p>Absolutely Loved the product!</p>
+          </div>
+          <div className="review">
+            <h3>Shishir</h3>
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
+            >
+              <Rating name="read-only" value={5} readOnly />
+            </Box>
+            <p>Absolutely Loved the product!</p>
+          </div>
+          <div className="review">
+            <h3>Shishir</h3>
+            <Box
+              sx={{
+                "& > legend": { mt: 2 },
+              }}
+            >
+              <Rating name="read-only" value={5} readOnly />
+            </Box>
+            <p>Absolutely Loved the product!</p>
           </div>
         </div>
       </div>
